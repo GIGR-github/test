2ec1e201ae2de69e0703b11e60435cec2183c444
Pavel Pan <ppanbeing@gmail.com>
2024-11-20 05:33:09 +0500

    Fix issues

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 public class LoginController {
     @GetMapping("/login")
     void login() {
         System.out.println("login");
+        System.out.println("looger is added");
     }
 }
